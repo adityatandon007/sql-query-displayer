b94d38e09844ba01277878d0ae469a86babe7d5d
adityatandon007 <adityatandon007@gmail.com>
2022-05-15 20:13:15 +0530

    adding tableSelector and ColumnSelector component

---
+++ src/components/TableSelector.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-card flat>
+    <v-card-text>
+      <span class="subtitle-1 my-5">SELECT FROM</span>
+      <v-select
+        :value="selectedTable"
+        dense
+        :items="tables"
+        label="Please select a table"
+        outlined
+        class="mt-5"
+        @change="setSelectedTable"
+      >
+      </v-select>
+      <v-divider class="mb-3 mt-3"></v-divider>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TableSelector",
+  props: {
+    tables: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    selectedTable: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    setSelectedTable(table) {
+      this.$emit("select", table);
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,73 @@
 <template>
-  <hello-world />
+  <div>
+    <table-selector
+      :tables="tables"
+      @select="setTable"
+      :selected-table="selectedTable"
+    />
+    <column-selector
+      :columns="tableColumns"
+      @update-columns="setColumns"
+      :selected-columns="selectedColumns"
+    />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+import TableSelector from "../components/TableSelector";
+import ColumnSelector from "../components/ColumnSelector";
+const columnsForTable1 = [
+  "Column A",
+  "Column B",
+  "Column C",
+  "Column D",
+  "Column E",
+  "Column F",
+];
+const columnsForTable2 = ["Column X", "Column Y", "Column Z"];
+const columnsForTable3 = [
+  "Column L",
+  "Column M",
+  "Column N",
+  "Column O",
+  "Column P",
+];
+const columnsForTable4 = ["Column Q", "Column R", "Column S", "Column T"];
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    TableSelector,
+    ColumnSelector,
+  },
+  data() {
+    return {
+      tables: ["Table 1", "Table 2", "Table 3", "Table 4"],
+      selectedTable: null,
+      selectedColumns: [],
+    };
+  },
+  computed: {
+    tableColumns() {
+      if (this.selectedTable === "Table 1") {
+        return columnsForTable1;
+      } else if (this.selectedTable === "Table 2") {
+        return columnsForTable2;
+      } else if (this.selectedTable === "Table 3") {
+        return columnsForTable3;
+      } else if (this.selectedTable === "Table 4") {
+        return columnsForTable4;
+      } else {
+        return [];
+      }
+    },
+  },
+  methods: {
+    setTable(table) {
+      this.selectedTable = table;
+    },
+    setColumns(columns) {
+      this.selectedColumns = [...columns];
+    },
   },
 };
 </script>
