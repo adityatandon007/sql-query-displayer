8b2ca692d8a848b4a36cfcaf380d5894df2c6283
adityatandon007 <adityatandon007@gmail.com>
2022-05-15 21:26:47 +0530

    adding an about section for the project description

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container>
+    <v-card class="pa-2">
+      <h1>SQL QUERY DISPLAYER</h1>
+      <v-list-item class="pl-0">It has a table selector to select tables.</v-list-item>
+      <v-list-item class="pl-0">It has a column selector to select columns. You can select all columns too.</v-list-item>
+      <v-list-item class="pl-0">You can also add filters.</v-list-item>
+      <v-list-item class="pl-0">You can also limit the number of records too.</v-list-item>
+    </v-card>
+    <v-card class="d-flex align-center font-weight-normal mt-4 pa-2">
+      <span class="mr-2">Note:</span>
+      <span>Currenty using dummy datasets</span>
+    </v-card>
+  </v-container>
 </template>
